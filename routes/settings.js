bad1c7975f082bf6b9c47cc8674502f53fe26ccf
Francis Gutierrez <ingutierrez.u@gmail.com>
2018-07-15 17:35:38 -0400

    add embeded heade v4

--- routes/settings.js
+++ routes/settings.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const shopName = req.query.shop;
+  const shopName = req.query.shop.split('.')[0];
 
   res.render('settings', { shopName });
 });
