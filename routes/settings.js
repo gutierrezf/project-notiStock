67836cf06376295f752f8834a2389c3ce0c2b3b0
Francis Gutierrez <ingutierrez.u@gmail.com>
2018-07-15 17:23:51 -0400

    add embeded heade v3

---
+++ routes/settings.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const shopName = req.query.shop;
+
+  res.render('settings', { shopName });
+});
