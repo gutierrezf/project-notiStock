7b1da4836c09fc46c3c1f7996c2f5321e0985678
Francis Gutierrez <ingutierrez.u@gmail.com>
2018-07-16 23:44:01 -0400

    add settings route and template

--- public/site.js
+++ public/site.js
@@ -1,7 +1,33 @@
+/* eslint-disable */
+
 function submitShop() {
-  var shop = $("div.install-form input").val();
+  var shop = $('div.install-form input').val();
   if (shop.length > 1) {
-    var q = "shop=" + shop;
+    var q = 'shop=' + shop;
     document.location.search = q;
   }
+}
+
+function saveSettings() {
+  var shop = $('body').data('shop');
+  var formData = {
+    shopName: shop
+  };
+
+  $('form.settings-form input').toArray().forEach(function(el) {
+    var name = $(el).attr('name');
+    var val = $(el).val();
+    formData[name] = val;
+  });
+
+  $.ajax({
+    url: '/settings',
+    type: "post",
+    data: formData,
+    success: function (res) {
+      if (res.success) {
+        window.location = '/?shop=' + shop;
+      }
+    }
+  });
 }
--- routes/settings.js
+++ routes/settings.js
@@ -4,8 +4,18 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const shopName = req.query.shop.split('.')[0];
+  const settings = {
+    email: 'admin@email.com',
+    storeLogo: ''
+  };
 
-  res.render('settings', { shopName });
+  res.render('settings', { shopName, settings });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+
+  res.send({ success: true, statusCode: 200 });
 });
 
 module.exports = router;
